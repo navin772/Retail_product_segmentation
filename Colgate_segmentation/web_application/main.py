2bda7395db7aa2ec3aac84681853bbc3007544a7
Navin Chandra <navinchandra772@gmail.com>
2023-07-26 14:26:00 +0530

    Added count and display number of detections in streamlit dashboard

--- Colgate_segmentation/web_application/main.py
+++ Colgate_segmentation/web_application/main.py
@@ -35,9 +35,12 @@ if side == 'Dashboard':
         # img = cv2.imread(uploaded_file)
         results = model.predict(img, stream=True)
 
+        count = 0
+
         for result in results:
             boxes = result.boxes.cpu().numpy() # get boxes on cpu in numpy
             for box in boxes: # iterate boxes
+                count += 1
                 r = box.xyxy[0].astype(int) # get corner points as int
                 print(r) # print boxes
                 cv2.rectangle(img, r[:2], r[2:], (0,255,0), 2) # draw boxes on img
@@ -47,6 +50,9 @@ if side == 'Dashboard':
         new_width = int(img.shape[1] * scale_percent / 100)
         new_height = int(img.shape[0] * scale_percent / 100)
         resized_img = cv2.resize(img, (new_width, new_height))
+
+        nums = f'Number of colgates in the image are: {count}'
+        st.markdown(nums)
 
         st.image(resized_img, caption='Predictions on the image', use_column_width=True)
 
